49ef4e1da5a9bcc845739ce2ecd6a21480dc8836
hotwords123 <hotwords123@gmail.com>
2020-04-18 19:18:46 +0800

    Add triple town demo

---
+++ triple-town-demo/precache-manifest.929d3326bb2437c38ff20f9fc1025816.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63bfe3cf9386c06924f844a3e30b56b6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4abb313dfdd98c0f69c4",
+    "url": "./static/css/main.953839a0.chunk.css"
+  },
+  {
+    "revision": "c7b275704602fdbfc61b",
+    "url": "./static/js/2.5d586d1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.5d586d1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4abb313dfdd98c0f69c4",
+    "url": "./static/js/main.7f2c188d.chunk.js"
+  },
+  {
+    "revision": "91665b3415358f760cfb",
+    "url": "./static/js/runtime-main.756a3c15.js"
+  }
+]);
